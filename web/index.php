906c73f1b72a8e4f81129bfe31cb93b09c9ba403
apsiqueira <apsiqueira.ADS@gmail.com>
2020-01-08 12:20:02 -0200

    Criacao Projetos

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once("visual/header.php");
+include_once("visual/body.php");
+include_once("visual/footer.php");
+
+
+?>
---
+++ web/visual/body.php
@@ -0,0 +1,32 @@
+<body>
+    <h1>Projeto Arduino</h1>
+    <p>Coleta de dados com arduino, usando placa node mcu</p>
+     <div class="distancia">
+     Sensor de Distancia: buscar do banco com php
+     
+     
+     </div>
+     <div class="temperatura">
+     Sensor de temperatura:  buscar do banco com php
+     
+     
+     </div>
+     <div class="Alerta">
+     Sensor de Alerta: buscar do banco com php
+     
+    <select name="sensor" id="sensor">
+    <option value="0">Ligado</option>
+    <option value="1">Desligado</option>
+    </select>
+     </div>
+
+
+
+   
+
+
+
+
+    
+</body>
+</html>
